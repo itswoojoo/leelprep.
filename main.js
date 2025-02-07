56027b2a2b5cf126ec5959d3ee65b62038a7bbda
itswoojoo <itswoojoo@gmail.com>
2025-02-07 18:14:32 +0900

    1페이지 글자 애니메이션,  2 페이지 캐러셀

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ $(document).ready(function() {
   });
 });
 
+
+//--------------------------------
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    // 화면의 절반 높이 계산
+    var scrollPos = $(window).scrollTop();
+    var page2Offset = $('#page2').offset().top;
+    var windowHeight = $(window).height();
+
+    // #page2가 화면 높이의 절반 이상 올라왔을 때
+    if (scrollPos + windowHeight / 2 > page2Offset) {
+      $('.main-content').addClass('close');
+    } else {
+      $('.main-content').removeClass('close');
+    }
+  });
+});
