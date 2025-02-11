ef06341ac7d74bc721d470bae65c5dd2c5b8270f
itswoojoo <itswoojoo@gmail.com>
2025-02-11 17:05:30 +0900

    page6 에 고객정보 넣는 페이지 만들어야함. page5 까지 했음

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
   // 페이지 새로 고침 시 자동으로 page1으로 스크롤 이동
-  $('html, body').animate({ scrollTop: $('#page1').offset().top }, 0);
+  $('html, body').animate({ scrollTop: $('#page1').offset().top }, 0);// 0은 즉시 이동을 의미
 });
 
 //------------------------
@@ -19,6 +19,9 @@ $(document).ready(function() {
     }
   });
 });
+
+//-------------------------------
+
 
 
 //--------------------------------
@@ -38,3 +41,98 @@ $(document).ready(function() {
     }
   });
 });
+
+//---------------------------------------------
+
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow', // Coverflow 효과 적용
+  loop: true,  // 슬라이드가 끝나면 처음으로 돌아옴
+  autoplay: {
+    delay: 3000, // 3초마다 자동으로 넘어감
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // 페이지네이션 클릭 가능
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  coverflowEffect: {
+    rotate: 50, // 슬라이드의 회전 각도
+    stretch: 0, // 슬라이드 간의 간격
+    depth: 100, // 3D 효과의 깊이
+    modifier: 1, // 효과의 강도
+    slideShadows: true, // 그림자 효과 추가
+  },
+});
+
+//----------------------------------
+
+$(document).ready(function () {
+  $(".close-btn").click(function (event) {
+    event.preventDefault(); // a 태그 클릭 방지
+    $(".mark").fadeOut(); // 부드럽게 사라지도록 설정
+  });
+});
+
+// $(document).ready(function () {
+//   $(".close-btn").click(function (event) {
+//     event.preventDefault(); // a 태그 클릭 방지
+//     $(".mark").toggleClass("small"); // 'small' 클래스를 추가/제거하여 크기 변경
+//   });
+// });
+
+//------------------------------------------
+
+$(document).ready(function(){
+  $(".map").click(function(){
+    $(this).toggleClass("zoomed");
+  });
+});
+
+
+//----------------------------------------------
+// $(document).ready(function() {
+//   $('.down-icon').on('click', function() {
+//     var target = '#page4'; // 이동할 페이지의 ID 또는 URL
+    
+//     var $buttonImg = $(this).find('img');
+
+//     // 슬라이드 애니메이션
+//     $buttonImg.animate({
+//       translateY: '10px' // 화살표를 아래로 내려줌
+//     }, 300, function() {
+//       // 애니메이션 끝난 후 페이지 이동
+//       $('html, body').animate({
+//         scrollTop: $(target).offset().top // 페이지로 스크롤 이동
+//       }, 500); // 0.5초 동안 부드럽게 이동
+//     });
+//   });
+// });
+
+$(document).ready(function() {
+  // 'next-page' 클래스를 가진 모든 버튼에 대해 클릭 이벤트 처리
+  $('.next-page').on('click', function() {
+    var currentPage = $(this).closest('.page'); // 현재 페이지
+    var nextPage = currentPage.next('.page');  // 다음 페이지
+
+    if (nextPage.length === 0) {  // 마지막 페이지인 경우
+      nextPage = $('.page').first(); // 처음 페이지로 돌아감
+    }
+
+    var $buttonImg = $(this).find('img');
+
+    // 슬라이드 애니메이션
+    $buttonImg.animate({
+      translateY: '10px' // 화살표를 아래로 내려줌
+    }, 300, function() {
+      // 애니메이션 끝난 후 페이지로 스크롤 이동
+      $('html, body').animate({
+        scrollTop: nextPage.offset().top // 다음 페이지로 스크롤 이동
+      }, 500); // 0.5초 동안 부드럽게 이동
+    });
+  });
+});
+
